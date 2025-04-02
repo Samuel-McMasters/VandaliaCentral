e3a65367dd82b03554b9f9210fbb277fb956df7c
Samuel-McMasters <sam.mcmasters@vandaliarental.com>
2025-04-02 08:53:47 -0400

    Added PDFGenerator service class

---
+++ VandaliaCentral/Services/PDFGenerator.cs
@@ -0,0 +1,31 @@
+﻿using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+
+using VandaliaCentral.Models;
+
+namespace VandaliaCentral.Services
+{
+    public class PDFGenerator
+    {
+        public static byte[] Generate(TerminationFormModel model)
+        {
+            return Document.Create(container =>
+            {
+                container.Page(page =>
+                {
+                    page.Size(PageSizes.A4);
+                    page.Margin(50);
+                    page.Content().Column(col =>
+                    {
+                        col.Item().Text("Employee Termination Form").FontSize(20).Bold().Underline().AlignCenter();
+                        col.Item().Text($"Employee Name: {model.EmployeeName}");
+                        col.Item().Text($"Termination Date: {model.TerminationDate:MMMM dd, yyyy}");
+                        col.Item().Text($"Reason:\n{model.Reason}");
+                    });
+                });
+
+            }).GeneratePdf();
+        }
+    }
+}
