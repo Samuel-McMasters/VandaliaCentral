a694c48046fbbf8b5bb882bfc6df26458af316f7
Samuel-McMasters <sam.mcmasters@vandaliarental.com>
2025-04-02 11:07:32 -0400

    all pdf fields added to TerminationFormModel, fields mapped to PDFTemplateFiller, pdf form is correctly emailed to user

--- VandaliaCentral/Services/PDFTemplateFiller.cs
+++ VandaliaCentral/Services/PDFTemplateFiller.cs
@@ -18,8 +18,31 @@ namespace VandaliaCentral.Services
             var fields = stamper.AcroFields;
 
             fields.SetField("EmployeeName", model.EmployeeName);
+            fields.SetField("EmployeeNumber", model.EmployeeNumber);
+            fields.SetField("Location", model.Location);
             fields.SetField("LastDateOfEmployment", model.LastDateOfEmployment.ToString("MM/dd/yyyy"));
-            //fields.SetField("Reason", model.Reason);
+            fields.SetField("Cellphone", model.Cellphone ? "Yes" : "Off");
+            fields.SetField("Notepad", model.Notepad ? "Yes" : "Off");
+            fields.SetField("DeviceAccounts", model.DeviceAccounts ? "Yes" : "Off");
+            fields.SetField("Uniforms", model.Uniforms ? "Yes" : "Off");
+            fields.SetField("CompanyVehicle", model.CompanyVehicle ? "Yes" : "Off");
+            fields.SetField("CreditCard", model.CreditCard ? "Yes" : "Off");
+            fields.SetField("Laptop", model.Laptop ? "Yes" : "Off");
+            fields.SetField("Hotspot", model.Hotspot ? "Yes" : "Off");
+            fields.SetField("DeviceLock", model.DeviceLock ? "Yes" : "Off");
+            fields.SetField("Keys", model.Keys ? "Yes" : "Off");
+            fields.SetField("PromotionalItems", model.PromotionalItems ? "Yes" : "Off");
+
+            fields.SetField("Microsoft365", model.Microsoft365 ? "Yes" : "Off");
+            fields.SetField("Vizion", model.Vizion ? "Yes" : "Off");
+            fields.SetField("Salesforce", model.Salesforce ? "Yes" : "Off");
+            fields.SetField("Certify", model.Certify ? "Yes" : "Off");
+            fields.SetField("RentalMan", model.RentalMan ? "Yes" : "Off");
+            fields.SetField("Telematics", model.Telematics ? "Yes" : "Off");
+            fields.SetField("Paylocity", model.Paylocity ? "Yes" : "Off");
+            fields.SetField("Ninety", model.Ninety ? "Yes" : "Off");
+            fields.SetField("Other", model.Other ? "Yes" : "Off");
+            fields.SetField("OtherText", model.OtherText);
 
             // Optional: flatten the form so fields can't be edited after filling
             stamper.FormFlattening = true;
