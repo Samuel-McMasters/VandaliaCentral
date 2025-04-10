035ea61653466f794bf4d5ad7222ef39686cf649
Samuel-McMasters <sam.mcmasters@vandaliarental.com>
2025-04-10 11:14:19 -0400

    Added function for employee change form and updated the employee change for page. Everything works in local testing. Publishing to azure for further testing

--- VandaliaCentral/Services/EmailsService.cs
+++ VandaliaCentral/Services/EmailsService.cs
@@ -60,5 +60,48 @@ namespace VandaliaCentral.Services
                 true // SaveToSentItems
             ).Request().PostAsync();
         }
+
+        public async Task SendChangeFormAsync(byte[] pdfBytes, string employeeName)
+        {
+            var message = new Message
+            {
+                Subject = $"Employee Change Form: {employeeName}",
+                Body = new ItemBody
+                {
+                    ContentType = BodyType.Text,
+                    Content = $"Attached is the Employee Change form for {employeeName}."
+                },
+                ToRecipients = new List<Recipient>
+                {
+                    new Recipient
+                    {
+                        EmailAddress = new EmailAddress
+                        {
+                            Address = "sam.mcmasters@vandaliarental.com" // TODO: Update this email
+                        }
+                    }
+                },
+                Attachments = new MessageAttachmentsCollectionPage
+                {
+                    new FileAttachment
+                    {
+                        Name = $"{employeeName}_change.pdf",
+                        ContentBytes = pdfBytes,
+                        ContentType = "application/pdf"
+                    }
+                }
+            };
+
+            await _graphClient.Me.SendMail(
+                new Message
+                {
+                    Subject = message.Subject,
+                    Body = message.Body,
+                    ToRecipients = message.ToRecipients,
+                    Attachments = message.Attachments
+                },
+                true // SaveToSentItems
+            ).Request().PostAsync();
+        }
     }
 }
