1d47bce9922fc1d69fbdebb0b0e4fb1a314c0937
Samuel-McMasters <sam.mcmasters@vandaliarental.com>
2025-04-16 12:49:29 -0400

    Added working IT form for ticket submission

--- VandaliaCentral/Services/GraphEmailService.cs
+++ VandaliaCentral/Services/GraphEmailService.cs
@@ -60,5 +60,38 @@ namespace VandaliaCentral.Services
                 .Request()
                 .PostAsync();
         }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string bodyText)
+        {
+            var graphClient = new GraphServiceClient(new DelegateAuthenticationProvider(async request =>
+            {
+                var token = await _tokenAcquisition.GetAccessTokenForUserAsync(new[] { "Mail.Send" });
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }));
+
+            var message = new Message
+            {
+                Subject = subject,
+                Body = new ItemBody
+                {
+                    ContentType = BodyType.Text,
+                    Content = bodyText
+                },
+                ToRecipients = new List<Recipient>
+        {
+            new Recipient
+            {
+                EmailAddress = new EmailAddress
+                {
+                    Address = toEmail
+                }
+            }
+        }
+            };
+
+            await graphClient.Me.SendMail(message, true)
+                .Request()
+                .PostAsync();
+        }
     }
 }
