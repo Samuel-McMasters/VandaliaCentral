c99ecb6ecaa7944acded42676b1504da612b37f0
Samuel-McMasters <sam.mcmasters@vandaliarental.com>
2025-04-17 09:44:00 -0400

    Following online guide for incremental consent. Applied changes to program.cs

--- VandaliaCentral/Program.cs
+++ VandaliaCentral/Program.cs
@@ -8,9 +8,6 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.Identity.Web.UI;
 using VandaliaCentral.Services;
 using QuestPDF.Infrastructure;
-using Microsoft.AspNetCore.Authentication.Cookies;
-
-
 
 var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
 
@@ -18,22 +15,14 @@ var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 builder.Services.AddSingleton<PdfService>();
 
-//For pdf api controller
+// For PDF API controller
 builder.Services.AddControllers();
 builder.Services.AddHttpClient();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddScoped<GraphUserService>();
 builder.Services.AddScoped<GraphEmailService>();
 
-//===================================================
-
-builder.Services.AddRazorPages(); // Needed for MicrosoftIdentity UI pages
-//builder.Services.Configure<CookieAuthenticationOptions>(CookieAuthenticationDefaults.AuthenticationScheme, options =>
-//{
-//    options.LogoutPath = "/MicrosoftIdentity/Account/SignOut";
-//});
-
-//===================================================
+// Microsoft Identity Web + Graph
 builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"))
     .EnableTokenAcquisitionToCallDownstreamApi()
@@ -41,8 +30,8 @@ builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
 
 builder.Services.Configure<CookieAuthenticationOptions>(CookieAuthenticationDefaults.AuthenticationScheme, options =>
 {
-    options.ExpireTimeSpan = TimeSpan.FromMinutes(20);        
-    options.SlidingExpiration = true;                        
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+    options.SlidingExpiration = true;
     options.Cookie.HttpOnly = true;
     options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
     options.Cookie.SameSite = SameSiteMode.Lax;
@@ -53,6 +42,12 @@ builder.Services.AddAuthorization(options =>
     options.FallbackPolicy = options.DefaultPolicy;
 });
 
+// Enables conditional access and consent handling for Graph calls
+builder.Services.AddServerSideBlazor()
+    .AddMicrosoftIdentityConsentHandler();
+
+builder.Services.AddRazorPages(); // Needed for MicrosoftIdentity UI pages
+
 builder.Services.AddScoped<GraphServiceClient>(serviceProvider =>
 {
     var tokenAcquisition = serviceProvider.GetRequiredService<ITokenAcquisition>();
@@ -61,47 +56,33 @@ builder.Services.AddScoped<GraphServiceClient>(serviceProvider =>
     {
         var accessToken = await tokenAcquisition.GetAccessTokenForUserAsync(
             new[] { "User.Read.All", "Mail.Send" });
+
         request.Headers.Authorization =
             new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
     }));
 });
-
-
-
-
-//===================================================
 
 var app = builder.Build();
 
 QuestPDF.Settings.License = LicenseType.Community;
 app.MapControllers();
 
-//Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. May want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-
-
-
-
-//===Middleware===================
+// Middleware
 app.UseHttpsRedirection();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
-//================================
-
-
 
 app.UseStaticFiles();
 app.UseAntiforgery();
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
-
 
 app.Run();
