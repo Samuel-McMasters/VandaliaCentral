993675eeed14752fa134bc93d847f20ee5554162
Samuel-McMasters <sam.mcmasters@vandaliarental.com>
2025-04-30 09:03:46 -0400

    Added CalendarEvent class

---
+++ VandaliaCentral/Models/CalendarEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace VandaliaCentral.Models
+{
+    public class CalendarEvent
+    {
+        public DateTime date {  get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+
+    }
+}
