3298a3207eb4b2b4869b62aaa663b08244b9f622
Samuel-McMasters <sam.mcmasters@vandaliarental.com>
2025-07-30 15:00:12 -0400

    add js folder and chatStream file

---
+++ VandaliaCentral/wwwroot/js/chatStream.js
@@ -0,0 +1,25 @@
+﻿window.streamChat = (url, messages, dotNetHelper) => {
+    const eventSource = new EventSourcePolyfill(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ messages })
+    });
+
+    eventSource.onmessage = (e) => {
+        const data = JSON.parse(e.data);
+        if (data.token) {
+            dotNetHelper.invokeMethodAsync("AppendToken", data.token);
+        }
+        if (data.done) {
+            eventSource.close();
+            dotNetHelper.invokeMethodAsync("FinishStreaming");
+        }
+    };
+
+    eventSource.onerror = (e) => {
+        console.error("Stream error", e);
+        eventSource.close();
+    };
+};
