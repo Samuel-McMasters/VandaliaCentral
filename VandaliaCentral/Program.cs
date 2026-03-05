a31d4f83b9b9b767b60d72e44dc5c98ae318c097
Samuel-McMasters <sam.mcmasters@vandaliarental.com>
2026-03-05 17:28:21 -0500

    Fix exam answer input state persistence in builder

--- VandaliaCentral/Program.cs
+++ VandaliaCentral/Program.cs
@@ -30,6 +30,7 @@ builder.Services.AddSingleton<CalendarService>();
 builder.Services.AddSingleton<ItNewsService>();
 builder.Services.AddSingleton<FlappyLeaderboardService>();
 builder.Services.AddSingleton<SowDocumentService>();
+builder.Services.AddSingleton<TrainingDocumentService>();
 builder.Services.AddSingleton<IPasswordGeneratorService, PasswordGeneratorService>();
 builder.Services.AddScoped<ISupportTicketSubmissionService, SupportTicketSubmissionService>();
 
